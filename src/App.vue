58a4e6b7c33946bf5845e3d54d248fcfe0cc7791
Vigneshwar <vickyravananrr@gmail.com>
2021-01-31 12:11:53 +0530

    menu layout design done

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <layout></layout>
+  </div>
+</template>
+
+<script>
+import layout from "./components/layout.vue";
+
+export default {
+  name: "App",
+  components: {
+    layout,
+  },
+};
+</script>
+
+<style></style>
